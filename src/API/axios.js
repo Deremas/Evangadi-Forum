858e38f2be19c48f3b8594603799a868e9de7d4e
Dereje Masresha <deremas27@gmail.com>
2025-06-05 14:54:48 +0300

    updated

--- src/API/axios.js
+++ src/API/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.VITE_BACKEND_API_BASE_URL,
+  baseURL: "http://18.206.214.228:3456/api",
+  // baseURL: process.env.VITE_BACKEND_API_BASE_URL,
   // baseURL: "https://evangadi-forum-starter-pw48.onrender.com/api",
   // baseURL: "http://localhost:3456/api",
 });
