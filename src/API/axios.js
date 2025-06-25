b0c839137d306679985e5d5252141bce877cc891
Dereje Masresha <deremas27@gmail.com>
2025-06-25 13:36:08 +0300

    deployment fix

--- src/API/axios.js
+++ src/API/axios.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://18.206.214.228:3456/api",
-  // baseURL: process.env.VITE_BACKEND_API_BASE_URL,
-  // baseURL: "https://evangadi-forum-starter-pw48.onrender.com/api",
-  // baseURL: "http://localhost:3456/api",
+  // baseURL: "http://18.206.214.228:3456/api",
+  baseURL: "https://evangadi-forum-starter-pw48.onrender.com/api",
+  // baseURL: "http://localhost:2224/api",
 });
 
 export default axiosInstance;
